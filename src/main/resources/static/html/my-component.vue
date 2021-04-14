b48b71502a41035f6440af7e0198f8df8f8fb3c6
MengZeWei <1163974499@qq.com>
2021-04-14 17:52:05 +0800

    收获地址管理

---
+++ src/main/resources/static/html/my-component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">Hello1 {{who}}</div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      who: 'world'
+    }
+  }
+}
+</script>
+
+<style>
+.hello {
+  background-color: #ffe;
+}
+</style>
