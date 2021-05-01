e0a26e886deb1129deb0f37e6841026170631cc7
MengZeWei <1163974499@qq.com>
2021-05-01 14:14:11 +0800

    完善购物车功能

--- src/main/java/com/bishe/bean/Test.java
+++ src/main/java/com/bishe/bean/Test.java
@@ -2,6 +2,11 @@ package com.bishe.bean;
 
 public class Test {
     public static void main(String[] args) {
-        Object t = new Object();
+        String USER_XZQH = "430102";
+        String sql = " select t.NAME_ SZ,z.NAME_ QX,t.ID_ from OS_GROUP t left join OS_GROUP z on t.PARENT_ID_ = z.GROUP_ID_\n" +
+                "where t.TYPE_ = '0' and t.RANK_LEVEL_ = 3 and t.ID_ LIKE '"+USER_XZQH+"%'\n" +
+                "order by t.ID_";
+
+        System.out.println(sql);
     }
 }
