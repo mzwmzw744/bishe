3698d7af41add617705cc4dc9e784e6b1c8d064f
YOUR NAME <1163974499@qq.com>
2021-05-07 18:42:22 +0800

    上传视频

--- src/main/java/com/bishe/controller/FileController.java
+++ src/main/java/com/bishe/controller/FileController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -30,43 +31,54 @@ public class FileController {
     UserMapper userMapper;
 
     @RequestMapping("/dynamicPic")
-    public String dynamicPic(@RequestParam String shoHeadPicUploadToken,@RequestPart("file") byte[] file) throws IOException {
+    public String dynamicPic(@RequestParam String shoHeadPicUploadToken,@RequestPart("file") byte[] file,@RequestPart("file") MultipartFile files) throws IOException {
         System.out.println(shoHeadPicUploadToken);
+        String name = files.getOriginalFilename();
+        int length = name.length();
+        String hz = name.substring(length-4, length);
         User user = (User)redisUtil.get(shoHeadPicUploadToken);
         String only = UUID.randomUUID()+"";
-
-        return getString(file, user, only);
+        return getString(file, user, only,hz);
     }
 
     @RequestMapping("/userHeadPicUpload")
-    public String userHeadPicUpload(@RequestParam String token,@RequestPart("file") byte[] file) throws IOException {
+    public String userHeadPicUpload(@RequestParam String token,@RequestPart("file") byte[] file,@RequestPart("file") MultipartFile files) throws IOException {
+        String name = files.getName();
+        int length = name.length();
+        String hz = name.substring(length-4, length);
         System.out.println(token);
         User user = (User)redisUtil.get(token);
         String only = UUID.randomUUID()+"";
-        String url = getString(file, user, only);
+        String url = getString(file, user, only,hz);
         user.setHeadPic(url);
         userMapper.userHeadPicUpdate(user);
-        return getString(file, user, only);
+        return getString(file, user, only,hz);
     }
 
     @RequestMapping("/shopDetailPic")
-    public String upload(@RequestParam String shoHeadPicUploadToken,@RequestPart("file") byte[] file) throws IOException {
+    public String upload(@RequestParam String shoHeadPicUploadToken,@RequestPart("file") byte[] file,@RequestPart("file") MultipartFile files) throws IOException {
+        String name = files.getName();
+        int length = name.length();
+        String hz = name.substring(length-4, length);
         User user = (User)redisUtil.get(shoHeadPicUploadToken);
         String only = UUID.randomUUID()+"";
-        return getString(file, user, only);
+        return getString(file, user, only,hz);
     }
 
     @RequestMapping("/updateShopHeadPic")
-    public String shopHeadPic(@RequestParam String shoHeadPicUploadToken,@RequestPart("file") byte[] file) throws IOException {
+    public String shopHeadPic(@RequestParam String shoHeadPicUploadToken,@RequestPart("file") byte[] file,@RequestPart("file") MultipartFile files) throws IOException {
+        String name = files.getName();
+        int length = name.length();
+        String hz = name.substring(length-4, length);
         User user = (User)redisUtil.get(shoHeadPicUploadToken);
         String only = UUID.randomUUID()+"";
-        return getString(file, user, only);
+        return getString(file, user, only,hz);
     }
 
-    private String getString(@RequestPart("file") byte[] file, User user, String only) throws IOException {
+    private String getString(@RequestPart("file") byte[] file, User user, String only,String hz) throws IOException {
         Service.createDir("upload\\userId"+user.getId());
-        String xdPath="upload\\"+"\\userId"+user.getId()+"\\"+only+".png";
-        String sqlHeadPicPath = "/upload"+"/userId"+user.getId()+"/"+only+".png";
+        String xdPath="upload\\"+"\\userId"+user.getId()+"\\"+only+hz;
+        String sqlHeadPicPath = "/upload"+"/userId"+user.getId()+"/"+only+hz;
         FileOutputStream fileOutputStream = new FileOutputStream(xdPath);
         FileCopyUtils.copy(file, fileOutputStream);
         return sqlHeadPicPath;
